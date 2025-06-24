af59c5881495c858b0add0159d49422a5e2718f6
RaffaeleCali <105224346+RaffaeleCali@users.noreply.github.com>
2025-06-24 16:35:36 +0200

    ccc

--- WasteVision/run2.sh
+++ WasteVision/run2.sh
@@ -1,18 +1,16 @@
 echo 'Installazione di kubectl...'
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl 
 
 echo 'Installazione di Docker...'
-sudo yum update -y && sudo yum install -y docker 
+sudo yum update -y
+sudo yum install -y docker 
 sudo systemctl start docker 
 sudo systemctl enable docker 
-sudo usermod -a -G docker $(whoami)
+sudo usermod -aG docker $(whoami)
 
-sg docker -c "
-
-    echo 'Installazione di Minikube...'
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
-    chmod +x minikube
-    sudo mv minikube /usr/local/bin/
-"
+echo 'Installazione di Minikube...'
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
+chmod +x minikube
+sudo mv minikube /usr/local/bin/minikube
